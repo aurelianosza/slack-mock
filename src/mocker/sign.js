e74aff4c183689923aab42e58130051f89c7ff4f
Angad Nadkarni <angad.nadkarni@gmail.com>
2020-08-07 15:49:45 +0530

    Fixes to logic

--- src/mocker/sign.js
+++ src/mocker/sign.js
@@ -3,23 +3,19 @@ const crypto = require('crypto');
 const qs = require('querystring');
 
 module.exports = (secret, request) => {
-  if (request.payload instanceof Object) {
-    request.payload = JSON.stringify(request.payload);
-  }
-
-  const version = 'v0';
-  const timestamp = Math.round(Date.now()/1000);
-  const body = qs.stringify(request, null, null, {format: 'RFC1738'});
-  const basestring = `${version}:${timestamp}:${body}`;
-
-  const hmac = crypto.createHmac('sha256', secret);
-  hmac.update(basestring);
-  const signature = hmac.digest('hex');
-
-  const headers = {
-    'X-Slack-Request-Timestamp': timestamp,
-    'X-Slack-Signature': signature,
-  }
-
-  return {headers, body};
+    const version = 'v0';
+    const timestamp = Math.round(Date.now() / 1000).toString();
+    const body = qs.stringify(request);
+    const basestring = `${version}:${timestamp}:${body}`;
+  
+    const hmac = crypto.createHmac('sha256', secret);
+    hmac.update(basestring);
+    const signature = hmac.digest('hex');
+  
+    const headers = {
+      'X-Slack-Request-Timestamp': timestamp,
+      'X-Slack-Signature': `${version}=${signature}`,
+    };
+  
+    return {headers, body};
 };
