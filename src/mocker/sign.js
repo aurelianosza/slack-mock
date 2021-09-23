f600825b7cf6e1d2a681ecdbbcd638301d83954b
Aureliano <aurelianosza@gmail.com>
2021-09-23 16:28:35 -0300

    adding quotation on body request.

--- src/mocker/sign.js
+++ src/mocker/sign.js
@@ -6,7 +6,7 @@ module.exports = (secret, request) => {
     const version = 'v0';
     const timestamp = Math.round(Date.now() / 1000).toString();
     const body = qs.stringify(request);
-    const basestring = `${version}:${timestamp}:${body}`;
+    const basestring = `${version}:${timestamp}:"${body}"`;
   
     const hmac = crypto.createHmac('sha256', secret);
     hmac.update(basestring);
